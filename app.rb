1729c5bf2bd7f9ae0e1bac98113e1f1ddde6e587
mcchama <aoe2_emi@hotmail.com>
2021-11-30 00:32:39 -0600

    Menu options
    A menu is displayed for the user experience

--- app.rb
+++ app.rb
@@ -2,10 +2,48 @@
 
 # Methods for the practice Game's Life
 class Game
-  def initialize(height, width)
+  def initialize(height = 6, width = 6)
     @height = height
     @width = width
+    @generation = 1
     @table = Array.new(height) { Array.new(width) }
+    generate_table
+  end
+
+  def show_menu
+    print 'Option: '
+    option = gets.to_i
+    case option
+    when 1
+      set_dimensions
+    when 2
+      show_table
+    when 3
+      next_generation
+      show_table
+    else
+      show_menu
+    end
+    show_menu
+  end
+
+  def menu_options
+    puts "Welcome to Life's Game. Please choose one of the next options"
+    puts "\t1) Set dimensions"
+    puts "\t2) Show current generation "
+    puts "\t3) Go a generation ahead "
+  end
+
+  def set_dimensions
+    print 'Introduce height: '
+    height = gets.to_i
+
+    print 'Introduce width: '
+    width = gets.to_i
+
+    initialize(height, width)
+
+    show_menu
   end
 
   def generate_table
@@ -43,6 +81,7 @@ class Game
   end
 
   def show_table
+    puts "#{@generation} generation"
     (0..@height).each do |i|
       print (@table[i]).to_s
       print "\n"
@@ -50,6 +89,7 @@ class Game
   end
 
   def next_generation
+    @generation += 1
     @height.times do |y|
       @width.times do |x|
         nc = neighbour_count(y, x)
@@ -68,18 +108,15 @@ class Game
     end
   end
 end
+system 'clear'
+games = Game.new(6, 6)
+games.menu_options
+games.show_menu
+# games = Game.new(height, width)
+# games.generate_table
+# puts 'Primera generacion'
+# games.show_table
 
-print 'Ingresa la altura: '
-height = gets.to_i
-print 'Ingresa la anchura: '
-width = gets.to_i
-puts
-
-games = Game.new(height, width)
-games.generate_table
-puts 'Primera generacion'
-games.show_table
-
-games.next_generation
-puts 'Segunda generacion'
-games.show_table
+# games.next_generation
+# puts 'Segunda generacion'
+# games.show_table
